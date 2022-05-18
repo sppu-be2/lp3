3c94fb59965c7f5170272d258c79400295da48ea
sppu-be2 <105779424+sppu-be2@users.noreply.github.com>
2022-05-18 14:50:15 +0530

    update DC

--- DC.py
+++ DC.py
@@ -21,7 +21,7 @@ regressor.fit(X.iloc[:,1:5],y)
 X_in=np.array([1,1,0,0])
 y_pred=regressor.predict([X_in])
 print("Prediction:", y_pred)
-from sklearn.externals.six import StringIO
+from six import StringIO
 from IPython.display import Image
 from sklearn.tree import export_graphviz
 import pydotplus
